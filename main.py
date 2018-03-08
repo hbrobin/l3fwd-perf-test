96053efeb20e1278c5259db3b4183282c7d079ef
Huang Bin <bin.huang@hxt-semitech.com>
2018-03-08 16:32:48 +0800

    call l3perf to run functions

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import session
+import l3_perf
 
 def main():
     hostname = '10.64.16.21'
     port = 22
     username = 'root'
-    password = 'root'
+    password = 'toor4hxt'
     execmd = "ofed_info -s"
 
     execmd21 = session.DirectSession(hostname, port, username, password)
-    execmd21.sshclient_execmd(execmd)
+    ver = execmd21.sshclient_execmd(execmd)
+    print(ver)
+    ver_str = str(ver, encoding="utf-8")
+    print(ver_str)
+    ver_ok = "MLNX_OFED_LINUX-4.4"
+    result = ver_ok in ver_str
+    print(result)
 
-    #sync21 = session.DirectSession(hostname, port, username, password)
-    #execmd21.sync_sw_repo("config.json")
-    session.upload_files("config.json")
+    l3_perf.run_l3_perf()
 
 if __name__ == "__main__":
     main()
