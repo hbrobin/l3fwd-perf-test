ced99a4332715d5a45781d21938c4d50b1770466
Huang Bin <bin.huang@hxt-semitech.com>
2018-03-09 17:45:08 +0800

    add remote bootup pktgen

--- l3_perf.py
+++ l3_perf.py
@@ -2,6 +2,7 @@ import paramiko
 import os
 import json
 import session
+# import pexpect
 
 class L3Perf:
     def __init__(self, l3_cfg_file):
@@ -84,9 +85,38 @@ class L3Perf:
                                   "export RTE_SDK=" + dpdk_dst_path + ";"
                                   "make -j 24")
 
+    def run_l3fwd(self, config_list):
+        print("Start forwarder.")
+
+    def run_pktgen(self, config_list):
+        for index in range(len(config_list)):
+            server_config = config_list[index]
+            if "xmit" == server_config["server_type"]:
+                print("Starting pktgen...")
+                # m = session.DirectSession(server_config['host_name'], server_config['host_port'],
+                #                           server_config['username'], server_config['password'])
+                server_config = config_list[index]
+                pktgen_path = (server_config["dpdk_path"] + (server_config["file_list"])["pktgen_pkg"]).rsplit('.', 2)[0]
+                pktgen_bin_name = (server_config["file_list"])["pktgen_pkg"].rsplit('-', 1)[0]
+                print("pktgen_path: " + pktgen_path + ", pktgen_bin_name: " + pktgen_bin_name)
+                # ssh = paramiko.SSHClient()
+                # ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+                # ssh.connect(hostname=server_config['host_name'], port=server_config['host_port'], username=server_config['username'], password=server_config['password'])
+                # channel = ssh.invoke_shell()
+
+                sh = session.ShellSession(server_config['host_name'], server_config['host_port'],
+                                           server_config['username'], server_config['password'])
+                execmd = "cd " + pktgen_path + ";" + "app/" + server_config["rte_target"] + "/" + pktgen_bin_name + " " + \
+                                            "-l 3-43 -n 6 -w 0000:01:00.0 -w 0003:01:00.0 -- -G -P --crc-strip -m [4:14-23].0 -m [24:34-43].1 -f themes/white-black.theme"
+                print(execmd)
+                output = sh.execute(execmd)
+                #output_str = str(output, encoding="utf-8")
+                print(output)
+
 
 
 def run_l3_perf():
     l3_m = L3Perf("config.json")
-    l3_m.upload_pkgs(l3_m.config_list)
-    l3_m.install_pkgs(l3_m.config_list)
+    # l3_m.upload_pkgs(l3_m.config_list)
+    # l3_m.install_pkgs(l3_m.config_list)
+    l3_m.run_pktgen(l3_m.config_list)
