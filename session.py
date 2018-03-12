650d3d7947dcf2eb5480dd9d8f697e52d8c841c6
Huang Bin <bin.huang@hxt-semitech.com>
2018-03-12 15:01:02 +0800

    add PktgenSession class

--- session.py
+++ session.py
@@ -2,6 +2,8 @@ import json
 import paramiko
 import os
 import re
+from threading import Thread
+
 
 
 
@@ -61,6 +63,80 @@ class DirectSession:
             transport.close()
             sftp.close()
 
+class PktgenSession:
+    def __init__(self, host, port, user, psw):
+        self.ssh = paramiko.SSHClient()
+        self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        self.ssh.connect(host, username=user, password=psw, port=port)
+
+        channel = self.ssh.invoke_shell()
+        self.stdin = channel.makefile('wb')
+        self.stdout = channel.makefile('r')
+
+    def __del__(self):
+        self.ssh.close()
+
+    def execute(self, cmd):
+        """
+
+        :param cmd: the command to be executed on the remote computer
+        :examples:  execute('ls')
+                    execute('finger')
+                    execute('cd folder_name')
+        """
+        cmd = cmd.strip('\n')
+        self.stdin.write(cmd + '\n')
+        # finish = 'end of stdOUT buffer. finished with exit status'
+        finish = 'version'
+        enter_cmd = '{}'.format(finish)
+        cmd_invalid = 'command not found'
+        pktgen_version = 'Version: DPDK'
+        self.stdin.write(enter_cmd + '\n')
+        shin = self.stdin
+        self.stdin.flush()
+
+        shout = []
+        sherr = []
+        exit_status = 0
+        for line in self.stdout:
+            if str(line).startswith(cmd) or str(line).startswith(enter_cmd):
+                # up for now filled with shell junk from stdin
+                shout = []
+            #elif str(line).startswith(finish):
+            elif cmd_invalid in str(line):
+                # our finish command ends with the exit status
+                # exit_status = int(str(line).rsplit(maxsplit=1)[1])
+                # if exit_status:
+                    # stderr is combined with stdout.
+                    # thus, swap sherr with shout in a case of failure.
+                sherr = shout
+                shout = []
+                break
+            elif pktgen_version in str(line):
+                sherr = shout
+                shout = []
+                break
+            else:
+                # get rid of 'coloring and formatting' special characters
+                # re_cmd = re.compile(r'(\x9B|\x1B\[)[0-?]*[ -/]*[@-~]').sub('', line).\
+                #              replace('\b', '').replace('\r', '')
+                shout.append(re.compile(r'(\x9B|\x1B\[)[0-?]*[ -/]*[@-~]').sub('', line).
+                             replace('\b', '').replace('\r', ''))
+
+                # print(re_cmd)
+
+        # first and last lines of shout/sherr contain a prompt
+        if shout and enter_cmd in shout[-1]:
+            shout.pop()
+        if shout and cmd in shout[0]:
+            shout.pop(0)
+        if sherr and enter_cmd in sherr[-1]:
+            sherr.pop()
+        if sherr and cmd in sherr[0]:
+            sherr.pop(0)
+
+        return str(line)
+
 class ShellSession:
     def __init__(self, host, port, user, psw):
         self.ssh = paramiko.SSHClient()
@@ -73,6 +149,21 @@ class ShellSession:
 
     def __del__(self):
         self.ssh.close()
+
+    @staticmethod
+    def _print_exec_out(cmd, out_buf, err_buf, exit_status):
+        print('command executed: {}'.format(cmd))
+        print('STDOUT:')
+        for line in out_buf:
+            print(line, end="")
+        print('end of STDOUT')
+        print('STDERR:')
+        for line in err_buf:
+            print(line, end="")
+        print('end of STDERR')
+        print('finished with exit status: {}'.format(exit_status))
+        print('------------------------------------')
+        pass
 
     def execute(self, cmd):
         """
@@ -97,19 +188,19 @@ class ShellSession:
             if str(line).startswith(cmd) or str(line).startswith(echo_cmd):
                 # up for now filled with shell junk from stdin
                 shout = []
-            # elif str(line).startswith(finish):
-            #     # our finish command ends with the exit status
-            #     exit_status = int(str(line).rsplit(maxsplit=1)[1])
-            #     if exit_status:
-            #         # stderr is combined with stdout.
-            #         # thus, swap sherr with shout in a case of failure.
-            #         sherr = shout
-            #         shout = []
-            #     break
-            # else:
-            #     # get rid of 'coloring and formatting' special characters
-            #     shout.append(re.compile(r'(\x9B|\x1B\[)[0-?]*[ -/]*[@-~]').sub('', line).
-            #                  replace('\b', '').replace('\r', ''))
+            elif str(line).startswith(finish):
+                # our finish command ends with the exit status
+                exit_status = int(str(line).rsplit(maxsplit=1)[1])
+                if exit_status:
+                    # stderr is combined with stdout.
+                    # thus, swap sherr with shout in a case of failure.
+                    sherr = shout
+                    shout = []
+                break
+            else:
+                # get rid of 'coloring and formatting' special characters
+                shout.append(re.compile(r'(\x9B|\x1B\[)[0-?]*[ -/]*[@-~]').sub('', line).
+                             replace('\b', '').replace('\r', ''))
 
         # first and last lines of shout/sherr contain a prompt
         if shout and echo_cmd in shout[-1]:
@@ -121,6 +212,7 @@ class ShellSession:
         if sherr and cmd in sherr[0]:
             sherr.pop(0)
 
+        self._print_exec_out(cmd=cmd, out_buf=shout, err_buf=sherr, exit_status=exit_status)
         return shin, shout, sherr
 
 def upload_files(cfg_file):
