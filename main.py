e4afc5cd29fd9242295dbb431ccbdbd1335bbf82
Huang Bin <bin.huang@hxt-semitech.com>
2018-04-02 13:57:25 +0800

    18.02 run ok

--- main.py
+++ main.py
@@ -2,20 +2,20 @@ import session
 import l3_perf
 
 def main():
-    hostname = '10.64.16.21'
-    port = 22
-    username = 'root'
-    password = 'toor4hxt'
-    execmd = "ofed_info -s"
-
-    execmd21 = session.DirectSession(hostname, port, username, password)
-    ver = execmd21.sshclient_execmd(execmd)
-    print(ver)
-    ver_str = str(ver, encoding="utf-8")
-    print(ver_str)
-    ver_ok = "MLNX_OFED_LINUX-4.4"
-    result = ver_ok in ver_str
-    print(result)
+    # hostname = '10.64.16.21'
+    # port = 22
+    # username = 'root'
+    # password = 'toor4hxt'
+    # execmd = "ofed_info -s"
+    #
+    # execmd21 = session.DirectSession(hostname, port, username, password)
+    # ver = execmd21.sshclient_execmd(execmd)
+    # print(ver)
+    # ver_str = str(ver, encoding="utf-8")
+    # print(ver_str)
+    # ver_ok = "MLNX_OFED_LINUX-4.4"
+    # result = ver_ok in ver_str
+    # print(result)
 
     l3_perf.run_l3_perf()
 
