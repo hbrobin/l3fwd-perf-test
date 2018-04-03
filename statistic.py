9cbf2b039ca702dcade52c6515b9ce59bb596240
Huang Bin <bin.huang@hxt-semitech.com>
2018-04-03 14:54:30 +0800

    add start/stop eth_stat thread

--- statistic.py
+++ statistic.py
@@ -6,11 +6,72 @@ import flowgen
 import l3fwd
 import time
 import nclib
+import threading
 # import pexpect
 
-class L3Perf:
-    def __init__(self, l3_cfg_file):
-        with open(l3_cfg_file) as config_file:
-            self.config_list = json.load(config_file)
+exitFlag = 0
+
+class Statistic:
+    def __init__(self, cfg_file):
+        with open(cfg_file) as config_file:
+            config_list = json.load(config_file)
             self.pktgen_cli = None
-        print("Load L3 perf configuration file success.")
+            for index in range(len(config_list)):
+                server_config = config_list[index]
+                if "xmit" == server_config["server_type"]:
+                    self.config = config_list[index]
+        print("Load flowgen configuration file success.")
+        self.m = session.DirectSession(self.config['host_name'], self.config['host_port'], self.config['username'],
+                                  self.config['password'])
+        statistic_src_path = self.config["repo_path"] + (self.config["pkg_list"])["eth_stat"]
+        statistic_dst_path = self.config["tool_path"]
+        self.m.sshclient_execmd("mkdir -p " + statistic_dst_path + ";"
+                                "cp " + statistic_src_path + " " + statistic_dst_path + ";"
+                                "chmod +rx " + statistic_dst_path + (self.config["pkg_list"])["eth_stat"])
+
+    def start_statistic(self):
+        threadList = ["PPS-Collection-Thread"]
+        # nameList = ["One", "Two", "Three", "Four", "Five"]
+        # queueLock = threading.Lock()
+        # workQueue = Queue.Queue(10)
+        threads = []
+        threadID = 1
+
+        # 创建新线程
+        for tName in threadList:
+            self.pps_thread = ppsThread(self, threadID, tName)
+            self.pps_thread.start()
+            threads.append(self.pps_thread)
+            threadID += 1
+
+    def stop_statistic(self):
+        global exitFlag
+        exitFlag = 1
+        self.pps_thread.terminate()
+
+
+class ppsThread(threading.Thread):
+    def __init__(self, statistic, threadID, name):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.statistic = statistic
+        # self.q = q
+
+    def run(self):
+        print("Starting " + self.name)
+        self.collect_data()
+        print("Exiting " + self.name)
+
+    def terminate(self):
+        pkill_cmd = "pkill eth_stat.sh"
+        self.statistic.m.execute(pkill_cmd)
+        print("terminate " + self.name)
+
+    def collect_data(self):
+        print("exitFlag = %d, %s processing..." % (exitFlag, self.name))
+        statistic_dst_path = self.statistic.config["tool_path"]
+        cmd = statistic_dst_path + self.statistic.config["pkg_list"]["eth_stat"]
+        print(cmd)
+        while not exitFlag:
+            self.statistic.m.execute(cmd)
