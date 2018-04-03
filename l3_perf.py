c6a7733e75c15fd64f98fd52257b7ef90eec03a2
Huang Bin <bin.huang@hxt-semitech.com>
2018-04-03 14:55:14 +0800

    1. add collect pps info 2. add try .. finally to exit l3 and pktgen

--- l3_perf.py
+++ l3_perf.py
@@ -4,6 +4,7 @@ import json
 import session
 import flowgen
 import l3fwd
+import statistic
 import time
 import nclib
 # import pexpect
@@ -16,21 +17,28 @@ class L3Perf:
         print("Load L3 perf configuration file success.")
 
 def run_l3_perf():
-    fg = flowgen.FlowGen("config.json")
-    fg.upload_pkgs()
-    fg.install_pkgs()
-    fg.run_pktgen()
-    fg.set_pktgen_range(64)
+    try:
+        fg = flowgen.FlowGen("config.json")
+        # fg.upload_pkgs()
+        # fg.install_pkgs()
+        fg.run_pktgen()
+        fg.set_pktgen_range(64)
 
-    l3 = l3fwd.L3Fwd("config.json")
-    l3.upload_pkgs()
-    l3.install_pkgs()
-    l3.run_l3fwd(1)
+        l3 = l3fwd.L3Fwd("config.json")
+        # l3.upload_pkgs()
+        # l3.install_pkgs()
+        l3.run_l3fwd(1)
 
-    fg.start_pktgen('1')
-    time.sleep(30)
-    fg.stop_pktgen('1')
-    fg.quit_pktgen()
-    l3.stop_l3fwd()
+        pps = statistic.Statistic("config.json")
+
+        fg.start_pktgen('1')
+        pps.start_statistic()
+        time.sleep(20)
+        pps.stop_statistic()
+        fg.stop_pktgen('1')
+    finally:
+        fg.quit_pktgen()
+        l3.stop_l3fwd()
+
 
 
