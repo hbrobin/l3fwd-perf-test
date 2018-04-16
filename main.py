0c65c756f49ee643a6e962507eff8c956dfc7cd5
Huang Bin <bin.huang@hxt-semitech.com>
2018-04-16 15:26:58 +0800

    add caller to both install()/unidirection()/bidirection() test

--- main.py
+++ main.py
@@ -15,9 +15,11 @@ def main():
     # print(ver_str)
     # ver_ok = "MLNX_OFED_LINUX-4.4"
     # result = ver_ok in ver_str
-    # print(result)
+    # print(result) `
 
-    l3_perf.run_l3_perf()
+    l3_perf.upload_install_l3_perf()
+    l3_perf.run_unidirection_l3_perf()
+    l3_perf.run_bidirection_l3_perf()
 
 if __name__ == "__main__":
     main()
